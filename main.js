5bbcd817d46499e17ee62e7842a892cabadb621c
Thomas-Basadonne <thommybasa@gmail.com>
2023-02-17 18:03:47 +0100

    milestone

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const { createApp } = Vue;
-
-createApp({
-  data() {
-    return {
-      message: "Hello Vue!",
-    };
-  },
-}).mount("#app");
+for (let i = 0; i < 10; i++) {
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((result) => {
+      console.log(result.data.response);
+    });
+}
