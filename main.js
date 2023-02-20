181a128726ee0d9ab6f4cc107f37f71e90907d08
Thomas-Basadonne <thommybasa@gmail.com>
2023-02-20 10:28:26 +0100

    final commit

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-for (let i = 0; i < 10; i++) {
-  axios
-    .get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((result) => {
-      console.log(result.data.response);
-    });
-}
+const app = Vue.createApp({
+  data() {
+    return {
+      casualEmails: [],
+    };
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((result) => {
+          this.casualEmails.push(result.data.response);
+        });
+    }
+  },
+});
+app.mount("#app");
